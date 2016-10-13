09510e0854383d980a417adc432f1c41e3088e01
andrewplan <a.plan88@gmail.com>
2016-10-13 12:24:03 -0500

    Copied over server-side code from other experiment without any console errors.

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require( 'express' );
+const BinaryServer = require( 'binaryjs' ).BinaryServer;
+const fs = require( 'fs' );
+const path = require( 'path' );
 const { json } = require( 'body-parser' );
 const googleSpeechConfig = require( './server/configs/googleSpeechConfig' );
-// const googleSpeechCredentials = require( './server/configs/googleSpeechCredentials.json' );
-const port = 4000;
+const wav = require( 'wav' );
+const lame = require( 'lame' );
+const outFile = 'demo.wav';
 
-const app = express();
-
-app.use( json() );
-app.use( express.static( `${ __dirname }/dist` ) );
-
-// [START speech_quickstart]
 // Imports the Google Cloud client library
 const Speech = require('google-cloud/node_modules/@google-cloud/speech');
 
@@ -18,30 +16,80 @@ const projectId = googleSpeechConfig.project_id;
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = './server/configs/googleSpeechCredentials.json';
 
-// Instantiates a client
-const speechClient = Speech({
-  projectId: projectId
-});
+const port = 4000;
 
-// The name of the audio file to transcribe
-const fileName = './server/resources/audio_recording_mainAudio_test_3.wav';
+const app = express();
 
-// The audio file's encoding and sample rate
-const options = {
-  encoding: 'LINEAR16',
-  sampleRate: 44000
-};
-
-// Detects speech in the audio file
-// speechClient.recognize(fileName, options, (err, result) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//
-//   console.log(`Transcription: ${result}`);
-// });
-// [END speech_quickstart]
-/******************* END Google Speech quickstart.js ********************/
+app.use( json() );
+app.use( express.static( `${ __dirname }/dist` ) );
 
 app.listen( port, () => { console.log( `Listening on ${ port }` ) } );
+
+binaryServer = BinaryServer({port: 9000});
+
+binaryServer.on('connection', function(client) {
+  console.log('new connection');
+
+  var fileWriter = new wav.FileWriter(outFile, {
+    channels: 1,
+    sampleRate: 44000,
+    bitDepth: 16
+  });
+
+  client.on('stream', function(stream, meta) {
+    console.log('new stream');
+    let streamClone = require( 'stream' );
+
+    let stream1 = stream.pipe( new streamClone.PassThrough() );
+    let stream2 = stream.pipe( new streamClone.PassThrough() );
+
+    stream1.pipe(fileWriter);
+
+    stream1.on('end', function() {
+        fileWriter.end();
+        console.log('wrote to file ' + outFile);
+
+        // Instantiates a client
+        const speechClient = Speech({
+          projectId: projectId
+        });
+
+        // The name of the audio file to transcribe
+        const fileName = outFile;
+
+        // The audio file's encoding and sample rate
+        const options = {
+          encoding: 'LINEAR16',
+          sampleRate: 44000
+        };
+
+        // Detects speech in the audio file
+        // speechClient.recognize(fileName, options, (err, result) => {
+        //   if (err) {
+        //     console.error(err);
+        //     return;
+        //   }
+        //
+        //   console.log(`Transcription: ${result}`);
+        // });
+    });
+
+    stream2
+      .pipe( new lame.Encoder( {
+          channels: 1
+          , bitDepth: 16
+          , float: false
+
+          , bitRate: 192
+          , outSampleRate: 44100
+          , mode: lame.STEREO
+        } ) )
+      .pipe( fs.createWriteStream( path.resolve( __dirname, 'demo.mp3' ) ) )
+      .on( 'close', () => { console.log( 'Done encoding to mp3' ); } );
+      // transcode file to mp3
+      // upload mp3 to Amazon S3
+      // call mongoDB method to POST obj with S3 URL and transcription
+          // then front end could make get request for the data posted to mongoDB
+      // delete wav from server
+  });
+});
